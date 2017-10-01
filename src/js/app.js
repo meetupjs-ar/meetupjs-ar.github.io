8639db3c0b3e4f27a71564b31c223d4579deb748
cristhian <durancristhian@gmail.com>
2017-10-01 15:49:08 -0300

    descomenta código útil

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 const calendar = require('./modules/calendar')
 const offline = require('./modules/offline')
 const menu = require('./modules/menu')
-// const serviceWorker = require('./modules/service-worker')
+const serviceWorker = require('./modules/service-worker')
 
 calendar()
 offline()
 menu()
-// serviceWorker()
+serviceWorker()
