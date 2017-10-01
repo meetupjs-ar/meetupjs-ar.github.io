23e13a21095548aedea711b2d988dbc530641f59
cristhian <durancristhian@gmail.com>
2017-10-01 16:05:05 -0300

    elimina offline.css

--- src/js/modules/offline.js
+++ src/js/modules/offline.js
@@ -1,13 +1,9 @@
 const toast = require('./toast.js')
 
-const body = document.querySelector('body')
-
 function updateConnectionStatus() {
-    const method = navigator.onLine ? 'remove' : 'add'
     const text = navigator.onLine ? 'Estás conectado' : 'Estás desconectado'
 
-    body.classList[method]('offline')
-    toast(text, icon)
+    toast(text)
 }
 
 module.exports = function offline() {
