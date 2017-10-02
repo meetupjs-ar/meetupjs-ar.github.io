ba737c9cf102faa72bfc5628cf63f6b76fd682d0
cristhian <durancristhian@gmail.com>
2017-10-01 21:09:43 -0300

    agrega indicador de página actual

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -2,6 +2,12 @@ const closeMenu = document.querySelector('#close-menu')
 const menu = document.querySelector('#menu')
 const openMenu = document.querySelector('#open-menu')
 
+function hightlightCurrentMenuItem() {
+    document
+        .querySelector(`#menu [href='${window.location.pathname}']`)
+        .classList.add('current-page')
+}
+
 function toggleMenuHandler() {
     closeMenu.classList.toggle('dn')
     menu.classList.toggle('dn')
@@ -12,4 +18,6 @@ function toggleMenuHandler() {
 module.exports = function menu() {
     openMenu.addEventListener('click', toggleMenuHandler)
     closeMenu.addEventListener('click', toggleMenuHandler)
+
+    hightlightCurrentMenuItem()
 }
