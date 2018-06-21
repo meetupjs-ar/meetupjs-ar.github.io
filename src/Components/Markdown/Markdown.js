782945a8ab37b4d5d24d9547e75aa3a59f0d591d
cristhian <durancristhian@gmail.com>
2018-06-21 10:42:49 -0300

    improve markdown-body

--- src/Components/Markdown/Markdown.js
+++ src/Components/Markdown/Markdown.js
@@ -1,8 +1,5 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
-import A from './Tags/A';
-import H1 from './Tags/H1';
-import H2 from './Tags/H2';
 
 class Markdown extends PureComponent {
   static props = {
@@ -13,14 +10,8 @@ class Markdown extends PureComponent {
     const { Content } = this.props;
 
     return (
-      <div className="black-alternative lh-copy">
-        <Content
-          components={{
-            a: A,
-            h1: H1,
-            h2: H2
-          }}
-        />
+      <div className="markdown-body">
+        <Content />
       </div>
     );
   }
