70417cd57a2de5af713c75e35eb4bcac747f47bb
cristhian <durancristhian@gmail.com>
2018-06-20 21:55:59 -0300

    another test with travis

--- deploy/git-config.sh
+++ deploy/git-config.sh
@@ -1,6 +1,8 @@
 ## initialize git
+echo 'initialize git'
 git config --global user.name "cristhian"
 git config --global user.email "durancristhian@gmail.com"
 
 # deploy npm script
+echo 'deploy npm script'
 npm run deploy
