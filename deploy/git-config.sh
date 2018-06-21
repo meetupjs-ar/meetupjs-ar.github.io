10e8b34ab3dd44d39c232e1303467e13f74c0655
cristhian <durancristhian@gmail.com>
2018-06-20 21:42:12 -0300

    fix deploy script

--- deploy/git-config.sh
+++ deploy/git-config.sh
@@ -1,3 +1,6 @@
-## Initialize git
-git config user.name "cristhian"
-git config user.email "durancristhian@gmail.com"
+## initialize git
+git config --global user.name "cristhian"
+git config --global user.email "durancristhian@gmail.com"
+
+# deploy npm script
+npm run deploy
