dc297257480775874a8dde9285bea54678e6dff9
cristhian <durancristhian@gmail.com>
2018-06-20 22:02:09 -0300

    update deploy to get more logging

--- deploy/git-config.sh
+++ deploy/git-config.sh
@@ -1,8 +1,6 @@
 ## initialize git
-echo 'initialize git'
 git config --global user.name "cristhian"
 git config --global user.email "durancristhian@gmail.com"
 
 # deploy npm script
-echo 'deploy npm script'
-npm run deploy
+NODE_DEBUG=gh-pages npm run deploy
