eee4fadbc8de636d99ae704ed800a438461311e7
cristhian <durancristhian@gmail.com>
2018-06-21 23:15:32 -0300

    improve blog files

---
+++ src/Components/Blog/Articles/como-hacemos-un-meetup.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Como hacemos un meetup',
+  authors: ['Camila Cepeda'],
+  publishedDay: '19/12/2017',
+  introduction:
+    'En este artículo te vamos a contar como organizamos los meetups, que pasos tenemos en cuenta, como nos contactamos con los oradores, empresas y sponsors.',
+  url: 'como-hacemos-un-meetup.html'
+};
---
+++ src/Components/Blog/Articles/consejos-para-dar-una-charla.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Consejos para dar una charla',
+  authors: ['Karen Serfaty', 'Cristhian Duran'],
+  publishedDay: '03/05/2018',
+  introduction:
+    'Les compartimos una lista de puntos que recomendamos tener en cuenta a la hora de preparar una charla.',
+  url: 'consejos-para-dar-una-charla.html'
+};
