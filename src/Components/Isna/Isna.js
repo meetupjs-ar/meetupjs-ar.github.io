84f88d58c076ee2fb7084723fa78134f8717630b
cristhian <durancristhian@gmail.com>
2018-06-22 20:24:34 -0300

    add notifications

--- src/Components/Isna/Isna.js
+++ src/Components/Isna/Isna.js
@@ -1,47 +1,67 @@
+import Mousetrap from 'mousetrap';
 import React, { PureComponent } from 'react';
 import LazyLoad from 'react-lazyload';
 import * as styles from './Isna.module.css';
-import IsnaPicture from './isna.png';
-import Mousetrap from 'mousetrap';
 import IsnaSound from './Isna.mp3';
+import IsnaPicture from './isna.png';
+import Notifications from './Notifications';
 
 class Isna extends PureComponent {
   state = {
     audio: new Audio(IsnaSound),
     active: false,
-    playing: false
+    message: ''
   };
 
   componentDidMount() {
-    Mousetrap.bind('up up down down left right left right b a enter', this.showeasterEgg);
+    Mousetrap.bind('up up down down left right left right b a enter', this.showEasterEgg);
+
+    const notification = Notifications.shift();
+    if (notification) this.showNotification(notification);
   }
 
-  showeasterEgg = () => {
-    if (!this.state.playing) {
+  showEasterEgg = () => {
+    if (!this.state.active) {
       this.setState({
-        active: true,
-        playing: true
+        active: true
       });
 
       this.state.audio.play();
 
       setTimeout(() => {
         this.setState({
-          active: false,
-          playing: false
+          active: false
         });
       }, 2500);
     }
   };
 
+  showNotification = (notification) => {
+    setTimeout(() => {
+      localStorage.setItem(notification.key, JSON.stringify(notification));
+
+      this.setState({
+        active: true,
+        message: notification.message
+      });
+
+      setTimeout(() => {
+        this.setState({
+          active: false
+        });
+      }, notification.hideAfter);
+    }, notification.activateAfter);
+  };
+
   render() {
-    const { active } = this.state;
+    const { active, message } = this.state;
 
     return (
       <div className={`${styles.easterEgg} ${active ? styles.easterEggActive : ''}`}>
         <LazyLoad height="100%" once={true}>
           <img src={IsnaPicture} alt="Nicolas Isnardi" className="v-btm w-100" />
         </LazyLoad>
+        {message && <p className={styles.easterEggMessage}>{message}</p>}
       </div>
     );
   }
---
+++ src/Components/Isna/Notifications.js
@@ -0,0 +1,39 @@
+import { isBefore } from 'date-fns';
+
+const allNotifications = [
+  {
+    activateAfter: 5000,
+    expiration: new Date(2018, 5, 20, 23, 59, 59),
+    hideAfter: 8000,
+    key: 'hello-world',
+    message: '¡Hola mundo!'
+  }
+];
+
+const notifications = isLocalStorageAvailable()
+  ? allNotifications.filter(byExpiration).filter(byStatusInStorage)
+  : [];
+
+function byExpiration(notification) {
+  if (isBefore(new Date(), notification.expiration)) {
+    return true;
+  } else {
+    try {
+      localStorage.removeItem(notification.key);
+    } catch (error) {
+      console.error(error);
+    }
+
+    return false;
+  }
+}
+
+function byStatusInStorage(notification) {
+  return !localStorage.getItem(notification.key);
+}
+
+function isLocalStorageAvailable() {
+  return !!window.localStorage;
+}
+
+export default notifications;
