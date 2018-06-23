8410ace3eb85afe18fff51239a36512df2e1bacf
cristhian <durancristhian@gmail.com>
2018-06-23 12:35:53 -0300

    remove useless dep

--- src/index.js
+++ src/index.js
@@ -10,11 +10,6 @@ if (process.env.NODE_ENV === 'production') {
   ReactGA.initialize('UA-93848949-1');
 }
 
-if (process.env.NODE_ENV === 'development') {
-  const { whyDidYouUpdate } = require('why-did-you-update');
-  whyDidYouUpdate(React);
-}
-
 smoothscroll.polyfill();
 ReactDOM.render(<Layout />, document.getElementById('root'));
 registerServiceWorker();
