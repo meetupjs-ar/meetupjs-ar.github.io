6691418dd404c592b466ff2dd0839e7a1741ec9a
cristhian <durancristhian@gmail.com>
2018-06-26 13:44:41 -0300

    update links in article

--- src/Pages/Blog/Articles/resena-meetup-junio-2018.js
+++ src/Pages/Blog/Articles/resena-meetup-junio-2018.js
@@ -4,7 +4,7 @@ export const metadata = {
   coverUrl: './resena-meetup-junio-2018/nicolas_isnardi.jpg',
   coverAlt: 'Nico Isnardi en su vuelta a Meetup.js',
   description:
-    'Te cuento que pasó durante la tarde del 13 de Junio que dio lugar a la segunda edición del año de Meetup.js en las oficinas de SCV Soft.',
+    'Segunda edición de Meetup.js del año, cargada de risas, reencuentros, emociones y buenas charlas.',
   publishedDay: new Date(2018, 5, 26),
   relativeUrl: 'articulos/resena-meetup-junio-2018.html',
   title: 'Reseña del Meetup.js de Junio 2018'
