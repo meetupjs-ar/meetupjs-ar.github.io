6e12e1dc6b0c3fca21890ffbb0724e89a42093a1
Cristhian Duran <durancristhian@gmail.com>
2018-09-11 02:07:22 -0300

    add animation to page transition

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,32 @@
+import Header from 'components/Header/Header';
+import { PageTransition } from 'next-page-transitions';
 import App, { Container } from 'next/app';
 import React from 'react';
-import Header from 'components/Header/Header';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import css from 'styled-jsx/css';
+
+const ANIMATION_DURATION = 400;
+const pageTransitionStyles = css`
+  .page-transition-enter {
+    opacity: 0;
+    transform: translate3d(0, 20px, 0);
+  }
+
+  .page-transition-enter-active {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+    transition: opacity ${ANIMATION_DURATION}ms, transform ${ANIMATION_DURATION}ms;
+  }
+
+  .page-transition-exit {
+    opacity: 1;
+  }
+
+  .page-transition-exit-active {
+    opacity: 0;
+    transition: opacity ${ANIMATION_DURATION}ms;
+  }
+`;
 
 export default class MyApp extends App {
   render() {
@@ -8,8 +34,13 @@ export default class MyApp extends App {
 
     return (
       <Container>
+        <style jsx global>
+          {pageTransitionStyles}
+        </style>
         <Header />
-        <Component {...pageProps} />
+        <PageTransition classNames="page-transition" timeout={ANIMATION_DURATION}>
+          <Component {...pageProps} />
+        </PageTransition>
       </Container>
     );
   }
