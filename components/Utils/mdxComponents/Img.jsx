16ea900f1f2bf68ce105da0a5814a8b41a0360b6
Cristhian Duran <durancristhian@gmail.com>
2018-09-15 11:22:31 -0300

    add shadow to post images

--- components/Utils/mdxComponents/Img.jsx
+++ components/Utils/mdxComponents/Img.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import css from 'styled-jsx/css';
 
 const fullWidthStyles = css`
+  .imageShadow {
+    box-shadow: 8px 8px var(--color-secondary);
+  }
+
   @media (min-width: 1024px) {
     .fullWidth {
       margin-left: -5rem;
@@ -14,7 +18,7 @@ const fullWidthStyles = css`
 const Img = ({ src, alt, ...rest }) => (
   <div className="fullWidth">
     <style jsx>{fullWidthStyles}</style>
-    <img src={src} alt={alt} {...rest} />
+    <img src={src} alt={alt} className="imageShadow" {...rest} />
   </div>
 );
 
