fc8b3f2081ba2048b3510c77ea9dd0e853fe71fb
Cristhian Duran <durancristhian@gmail.com>
2018-09-15 01:17:36 -0300

    add method to check if we need to show the easter egg

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,8 @@
 import Header from 'components/Header/Header';
 import { PageTransition } from 'next-page-transitions';
 import App, { Container } from 'next/app';
+import { withRouter } from 'next/router';
+import PropTypes from 'prop-types';
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import css from 'styled-jsx/css';
@@ -28,9 +30,26 @@ const pageTransitionStyles = css`
   }
 `;
 
-export default class MyApp extends App {
+class MyApp extends App {
+  static propTypes = {
+    router: PropTypes.shape({
+      pathname: PropTypes.string.isRequired
+    }).isRequired
+  };
+
+  showIsna = () => {
+    const url = this.props.router.pathname;
+
+    if (url.includes('blog') || url.includes('articulos')) {
+      return false;
+    }
+
+    return true;
+  };
+
   render() {
     const { Component, pageProps } = this.props;
+    // const showIsna = this.showIsna();
 
     return (
       <Container>
@@ -45,3 +64,5 @@ export default class MyApp extends App {
     );
   }
 }
+
+export default withRouter(MyApp);
