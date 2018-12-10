dff86357acacacf8366c98272743fa8ba9e74301
Cristhian Duran <durancristhian@gmail.com>
2018-12-09 22:28:30 -0300

    Add november review

---
+++ components/Blog/Articles/resena-meetup-noviembre-2018.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  authors: ['Leonardo Galante'],
+  // TODO: complete cover and coverAlt
+  coverUrl: '/static/Blog/resena-meetup-noviembre-2018/cover.jpg',
+  coverAlt: '',
+  description:
+    'Cumplimos 50 eventos y lo festejamos encontrándonos en Medallia donde tuvimos 5 charlas y el networking de siempre.',
+  publishedDay: new Date(2018, 11, 11),
+  relativeUrl: 'articulos/resena-meetup-noviembre-2018.html',
+  title: 'Reseña del Meetup.js de Noviembre 2018'
+};
