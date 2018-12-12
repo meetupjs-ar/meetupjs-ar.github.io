240f473edf32e74e4a0fe90bf0227f015f7915d4
Cristhian Duran <durancristhian@gmail.com>
2018-12-12 17:36:14 -0300

    highligth devday

--- components/Utils/eventMustBeHighlighted.js
+++ components/Utils/eventMustBeHighlighted.js
@@ -1,10 +1,13 @@
 export default function eventMustBeHighlighted(eventName) {
   const eventNameUppercased = eventName.toUpperCase();
+
+  const devDayArUppercased = 'DevDayAr'.toUpperCase();
   const meetupJsUppercased = 'Meetup.js'.toUpperCase();
   const nodeConfArgentinaUppercased = 'NodeConf Argentina'.toUpperCase();
 
   return (
     eventNameUppercased.includes(meetupJsUppercased) ||
-    eventNameUppercased.includes(nodeConfArgentinaUppercased)
+    eventNameUppercased.includes(nodeConfArgentinaUppercased) ||
+    eventNameUppercased.includes(devDayArUppercased)
   );
 }
