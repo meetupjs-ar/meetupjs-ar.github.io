7fc398eac217c1931a0b3fb86a8babd36c06cd81
Cristhian Duran <durancristhian@gmail.com>
2018-12-28 17:06:15 -0300

    Fix in calendar

--- components/Calendar/DayFooter.jsx
+++ components/Calendar/DayFooter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const DayFooter = ({ dayNumber, dayName, isToday, strike }) => (
   <div className="black-alternative tr-l w-20 w-100-l">
-    <span className={`${isToday ? 'dark-green' : ''} ${strike ? 'strike' : ''} f3`}>
+    <span className={`${isToday ? 'dark-green' : ''} ${strike ? 'strike black-50' : ''} f3`}>
       {dayNumber > 9 ? dayNumber : `0${dayNumber}`}
     </span>
     <span className={`${isToday ? 'dark-green' : ''} db dn-l f7 ttc`}>{dayName}</span>
