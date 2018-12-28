8d4674904925e7e54e768580cc599ba4ecfbcd0c
Cristhian Duran <durancristhian@gmail.com>
2018-12-28 02:26:33 -0300

    add year separation

--- components/Blog/BlogPage.jsx
+++ components/Blog/BlogPage.jsx
@@ -15,7 +15,22 @@ import { metadata as ResenaMeetupSeptiembreMetadata } from './Articles/resena-me
 import { metadata as ResenaMeetupOctubreMetadata } from './Articles/resena-meetup-octubre-2018.mdx';
 import { metadata as ResenaMeetupNoviembreMetadata } from './Articles/resena-meetup-noviembre-2018.mdx';
 
-const publishedArticles = [
+function splitByYear(articles) {
+  return articles.reduce((prev, curr) => {
+    const articleYear = curr.publishedDay.getFullYear();
+
+    if (!prev[articleYear]) {
+      // eslint-disable-next-line no-param-reassign
+      prev[articleYear] = [];
+    }
+
+    prev[articleYear].push(curr);
+
+    return prev;
+  }, {});
+}
+
+const allArticles = [
   ComoHacemosUnMeetupMetadata,
   ConsejosParaDarUnaCharlaMetadata,
   ConsejosParaDarUnaLightningMetadata,
@@ -28,42 +43,52 @@ const publishedArticles = [
   ResenaMeetupOctubreMetadata,
   ResenaMeetupNoviembreMetadata
 ].sort((a, b) => b.publishedDay - a.publishedDay);
+const articlesByYear = splitByYear(allArticles);
+const years = Object.keys(articlesByYear)
+  .map(Number)
+  .sort((y1, y2) => y2 - y1);
 
 class BlogPage extends Component {
   constructor(props) {
     super(props);
 
-    this.articlesMetadata = [...publishedArticles];
+    this.articlesMetadata = { ...articlesByYear };
+    this.years = [...years];
   }
 
   render() {
     return (
       <Container>
         <h1 className="mv0 tc">Blog</h1>
-        {this.articlesMetadata.map(metadata => (
-          <div className="mt4" key={metadata.title}>
-            <Link href={metadata.relativeUrl} passHref>
-              <a
-                href="#!"
-                className="black-alternative bg-animate br2 db bg-white hover-bg-washed-yellow no-underline overflow-hidden"
-              >
-                {metadata.coverUrl && (
-                  <img src={metadata.coverUrl} alt={metadata.coverAlt} className="v-btm" />
-                )}
-                <div className={`${metadata.coverUrl ? 'bb bl br' : 'ba'} b--black-10 bw1 pa3`}>
-                  <h2 className="f4 f3-ns mb3 mt0">{metadata.title}</h2>
-                  <p className="black-alternative-light f6 mb0 mt3">
-                    <span>Por </span>
-                    <strong>{formatAuthors(metadata.authors)}</strong>
-                    <span>. Publicado el </span>
-                    <FormatDate date={metadata.publishedDay} />
-                    <span>.</span>
-                  </p>
-                  <p className="mv3">{metadata.description}</p>
-                </div>
-              </a>
-            </Link>
-          </div>
+        {this.years.map(year => (
+          <React.Fragment key={year}>
+            <h3 className="black-50 f4 f3-ns mt4">{year}</h3>
+            {this.articlesMetadata[year].map(metadata => (
+              <div className="mt4" key={metadata.title}>
+                <Link href={metadata.relativeUrl} passHref>
+                  <a
+                    href="#!"
+                    className="black-alternative bg-animate br2 db bg-white hover-bg-washed-yellow no-underline overflow-hidden"
+                  >
+                    {metadata.coverUrl && (
+                      <img src={metadata.coverUrl} alt={metadata.coverAlt} className="v-btm" />
+                    )}
+                    <div className={`${metadata.coverUrl ? 'bb bl br' : 'ba'} b--black-10 bw1 pa3`}>
+                      <h2 className="f4 f3-ns mb3 mt0">{metadata.title}</h2>
+                      <p className="black-alternative-light f6 mb0 mt3">
+                        <span>Por </span>
+                        <strong>{formatAuthors(metadata.authors)}</strong>
+                        <span>. Publicado el </span>
+                        <FormatDate date={metadata.publishedDay} />
+                        <span>.</span>
+                      </p>
+                      <p className="mv3">{metadata.description}</p>
+                    </div>
+                  </a>
+                </Link>
+              </div>
+            ))}
+          </React.Fragment>
         ))}
       </Container>
     );
