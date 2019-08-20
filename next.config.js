5d0293e51ad8416b7474fccea28b3a726993e239
Cristhian Duran <durancristhian@gmail.com>
2019-08-20 00:29:39 -0300

    Improvec2

--- next.config.js
+++ next.config.js
@@ -31,6 +31,7 @@ module.exports = withPlugins(
       new PacktrackerPlugin({
         project_token: '6520c5cc-d69d-4c49-9b1b-e66fbdcabc4f',
         upload: true,
+        fail_build: true,
         branch: process.env.TRAVIS_BRANCH || process.env.TRAVIS_PULL_REQUEST_BRANCH
       })
     ]
