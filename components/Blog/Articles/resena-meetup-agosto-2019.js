e2323444395f6aa51fb8e54c0c1ad0aa3fddeba3
Cristhian Duran <durancristhian@gmail.com>
2019-09-01 23:52:05 -0300

    Subo la versión desde el repo para trabajar mejor la review

---
+++ components/Blog/Articles/resena-meetup-agosto-2019.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  authors: ['David Rene Comba Lareu'],
+  /* TODO: completar con un alt para la foto `coverUrl` */
+  coverUrl: '/static/Blog/resena-meetup-agosto-2019/meetupjs_aug19_01.jpg',
+  /* TODO: completar con un alt para la foto `coverUrl` */
+  coverAlt: '',
+  description: 'Este evento nos voló la capocha con SvelteJS y Vue 3',
+  /* TODO: completar con la fecha real de publicación */
+  publishedDay: new Date(2019, 7, 29),
+  relativeUrl: '/articulos/resena-meetup-agosto-2019.html',
+  title: 'Reseña del Meetup.js de agosto de 2019'
+};
