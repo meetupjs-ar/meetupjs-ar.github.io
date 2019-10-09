b1f57c6ebd559ab8321ee1e694a8415bf333f075
Mateo Silguero <mfsilguero@gmail.com>
2019-10-09 12:41:48 -0300

    Update resena-meetup-septiembre-2019.js

--- components/Blog/Articles/resena-meetup-septiembre-2019.js
+++ components/Blog/Articles/resena-meetup-septiembre-2019.js
@@ -1,7 +1,7 @@
 export const metadata = {
 	authors: ["Manuel Zambrano"],
 	coverUrl: "/static/Blog/resena-meetup-septiembre-2019/meetupjs_sep19_10.jpg",
-	coverAlt: "",
+	coverAlt: "Todo el mundo expectante al Kahoot",
 	description: "Easy peasy con robots y unit testing",
 	publishedDay: new Date(2019, 10, 10),
 	relativeUrl: "/articulos/resena-meetup-septiembre-2019.html",
