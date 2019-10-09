4609ece892b551912ebd1a97e0ee801ee4097b2f
Cristhian Duran <durancristhian@gmail.com>
2019-10-09 12:35:19 -0300

    Update components/Blog/Articles/resena-meetup-septiembre-2019.js

--- components/Blog/Articles/resena-meetup-septiembre-2019.js
+++ components/Blog/Articles/resena-meetup-septiembre-2019.js
@@ -3,7 +3,7 @@ export const metadata = {
 	coverUrl: "/static/Blog/resena-meetup-septiembre-2019/meetupjs_sep19_10.jpg",
 	coverAlt: "",
 	description: "Easy peasy con robots y unit testing",
-	publishedDay: new Date(2019, 10, 8),
+	publishedDay: new Date(2019, 10, 10),
 	relativeUrl: "/articulos/resena-meetup-septiembre-2019.html",
 	title: "Reseña del Meetup.js de septiembre de 2019"
 };
