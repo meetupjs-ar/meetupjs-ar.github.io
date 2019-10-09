a51f856283129d32b51f91fdf031be5fdcc02f84
Cristhian Duran <durancristhian@gmail.com>
2019-10-09 12:34:49 -0300

    Update components/Blog/Articles/resena-meetup-septiembre-2019.js

--- components/Blog/Articles/resena-meetup-septiembre-2019.js
+++ components/Blog/Articles/resena-meetup-septiembre-2019.js
@@ -1,6 +1,6 @@
 export const metadata = {
 	authors: ["Manuel Zambrano"],
-	coverUrl: "/static/Blog/resena-meetup-septiembre-2019/meetupjs_sep19_2.jpg",
+	coverUrl: "/static/Blog/resena-meetup-septiembre-2019/meetupjs_sep19_10.jpg",
 	coverAlt: "",
 	description: "Easy peasy con robots y unit testing",
 	publishedDay: new Date(2019, 10, 8),
